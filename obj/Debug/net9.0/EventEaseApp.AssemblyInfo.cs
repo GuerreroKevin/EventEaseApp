d6acc542b6a4ec17b9755a4a7a4030ac8739ca16
GuerreroKevin <kevin.gro.99@gmail.com>
2025-11-19 11:49:52 -0700

    Activity 2: Optimize the app performance and reliability

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78f528ac466620beebce11fee9151e6cc502978a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db65ff7fae5464a6906b81db634643da46766995")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
